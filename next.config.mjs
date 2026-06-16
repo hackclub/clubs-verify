2c54bed2fda9c57986d2e885bd89e1cd0734449f
whatbeato <96067139+whatbeato@users.noreply.github.com>
2026-06-16 10:38:58 +0100

    added sentry error monitoring because i don't trust myself

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import { withSentryConfig } from "@sentry/nextjs";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -8,4 +10,11 @@ const nextConfig = {
   distDir: process.env.NEXT_DIST_DIR || ".next",
 };
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+  org: process.env.SENTRY_ORG,
+  project: process.env.SENTRY_PROJECT,
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+  widenClientFileUpload: true,
+  tunnelRoute: "/monitoring",
+  silent: !process.env.CI,
+});
